ebf47e694973babd23dd55b706c1531a7fdab37e
pashahaha <156494753+pashahaha@users.noreply.github.com>
2024-02-24 12:26:59 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+# Dan inilah cara Kamu mengganti nama file dari variabel!
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+                    picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+sampah_organik = ["daun", "kertas", "sisa makanan"]
+sampah_nonorganik = ["plastik", "kaleng", "kabel"]
+
+@bot.command()
+async def pilih_sampah(ctx):
+    await ctx.send("masukkan jenis sampah: ")
+    msg = await bot.wait_for("message")
+    if msg.content in sampah_organik:
+        await ctx.send("Masukkan ke sampah organik")
+    elif msg.content in sampah_nonorganik:
+        await ctx.send("Masukkan ke sampah non-organik")
+
+bot.run("MTIwMDY2OTgzMjk2MjY0NjAyNg.GZ7jwZ.9X8sinfkyVcoOb-OHzPDwU7y-MIL4JDhP97tHQ")
